8a95feb50e13436a26375dc404dca2df4be00ce5
Matthew Doles <matteddied@gmail.com>
2021-04-17 13:48:07 -0400

    Abstract HomeStack navigator, adjust header styles

---
+++ stacks/default-options.js
@@ -0,0 +1,19 @@
+import colors from 'consts/colors';
+
+export default {
+  headerStyle: {
+    backgroundColor: colors.primaryColor,
+    height: 100,
+    borderBottomColor: colors.white,
+    borderBottomWidth: 1,
+  },
+  headerTitleStyle: {
+    color: 'white',
+    fontSize: 22,
+  },
+  headerBackTitleStyle: {
+    color: 'white',
+  },
+  headerTintColor: 'white',
+  fontSize: 22,
+};
