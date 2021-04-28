5e5609c907f884499260293b7f93dd152a1cc940
Matthew Doles <matteddied@gmail.com>
2021-04-27 21:09:48 -0400

    Create ProfileScreen, query and list user's completed quizes

--- stacks/ProfileStack/index.js
+++ stacks/ProfileStack/index.js
@@ -1,23 +1,41 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import defaultOptions from 'stacks/default-options';
-import { LOGIN_SCREEN } from 'screens/routes';
+import { LOGIN_SCREEN, PROFILE_SCREEN } from 'screens/routes';
 import Login from 'screens/Login';
+import Profile from 'screens/Profile';
+import { AppContext } from 'context';
 
 const Stack = createStackNavigator();
 
 export default function ProfileStack() {
+  const { user } = useContext(AppContext);
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        options={{
-          ...defaultOptions,
-          title: 'Login',
-        }}
-        name={LOGIN_SCREEN}
-        component={Login}
-      />
+      {user !== null ? (
+        <Stack.Screen
+          name={PROFILE_SCREEN}
+          component={Profile}
+          initialParams={{
+            displayName: user.displayName,
+            userId: user.uid,
+          }}
+          options={{
+            ...defaultOptions,
+            title: 'Profile',
+          }}
+        />
+      ) : (
+        <Stack.Screen
+          options={{
+            ...defaultOptions,
+            title: 'Login',
+          }}
+          name={LOGIN_SCREEN}
+          component={Login}
+        />
+      )}
     </Stack.Navigator>
   );
 }
